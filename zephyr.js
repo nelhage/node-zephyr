8a519a0ef309244c58d38b9f8ba2255609d2579d
Nelson Elhage <nelhage@nelhage.com>
2011-10-14 01:49:18 -0400

    Use an Error subclass for Zephyr errors.

--- zephyr.js
+++ zephyr.js
@@ -1,16 +1,20 @@
 var _zephyr = require('./build/default/_zephyr');
 
-
 var connection = null;
+
+function ZephyrError (code) {
+    this.message = _zephyr.error_message(code);
+    this.code = code;
+}
+
+ZephyrError.prototype = new Error();
+ZephyrError.prototype.constructor = ZephyrError;
 
 module.exports.get_connection = function () {
     if (connection == null) {
         connection = new _zephyr.Connection
         connection.on("zephyr_error", function (errno) {
-            connection.emit('error', {
-                errno: errno,
-                error: _zephyr.error_message(errno)
-            })
+            connection.emit('error', new ZephyrError(errno));
         })
     }
     return connection;
